f5a8b649372c2f08e80478e12b495068ff52f508
Matt Culbreth <mattculbreth@gmail.com>
2014-02-23 22:58:13 -0500

    * added requirements.txt * added .gitignore

--- Vagrant-setup/bootstrap.sh
+++ Vagrant-setup/bootstrap.sh
@@ -3,10 +3,11 @@
 apt-get update
 
 # build environment
-apt-get -y install gcc build-essential git
+apt-get -y install gcc build-essential python-dev git
 
 # python
 apt-get -y install python-pip python-virtualenv
+cd /vagrant && virtualenv . && source bin/activate && pip install -r requirements.txt
 
 # golang
 wget https://go.googlecode.com/files/go1.2.linux-amd64.tar.gz
